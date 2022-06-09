7bf44a9e8c9a5a34d3e64ad91bac024a7f4bd535
DogGang <rlaehgusqp@naver.com>
2022-06-10 03:32:04 +0900

    commit

--- header.js
+++ header.js
@@ -13,3 +13,19 @@ $(function(){
        return window.pageYOffset || document.documentElement.scrollTop;
        }
    });
+
+
+   $(document).ready(function(){
+        $('.menu_btn>a').on('click', function(){
+        $('.menu_bg').show(); 
+        $('.sidebar_menu').show().animate({
+           right:0
+        });   
+        });
+        $('.close_btn>a').on('click', function(){
+        $('.menu_bg').hide(); 
+        $('.sidebar_menu').animate({
+           right: '-' + 50 + '%'
+                    },function(){$('.sidebar_menu').hide(); }); 
+        });
+   });
